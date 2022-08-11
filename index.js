5b75bce9de6d4ba4b1706d856f3d43130af59f8f
langfalvidavid <langfalvi.david@gmail.com>
2022-08-11 14:56:55 +0200

    bubble sort working

--- index.js
+++ index.js
@@ -19,8 +19,7 @@ const arrayElements = document.getElementById("array-el")
 function render(arr){
 for(let j=0;j<arr.length;j++){
   arrayElements.insertAdjacentHTML("beforeend", arr[j].htmlEl)
-  const generatedDiv = document.getElementById("generated-el-" + j)
-  console.log(generatedDiv)
+  const generatedDiv = document.getElementById("generated-el@" + arr[j].id)
   generatedDiv.style.width = `${arr[j].val/5}%`
   generatedDiv.style.maxHeight = `20%`
   generatedDiv.style.height = 90/slider.value+"%"
@@ -33,14 +32,15 @@ function fillArray(){
 for(let i=0;i<slider.value;i++){
   const newRandom = randomNum()
   
-    const generatedDiv = {htmlEl:`<div id="generated-el-${i}" class="generated-elements">${newRandom}</div>`,
-                          val: newRandom}
+    const generatedDiv = {htmlEl:`<div id="generated-el@${i}" class="generated-elements">${newRandom}</div>`,
+                          val: newRandom,
+                          id: i}
     divs.push(generatedDiv)
 }}
 function removeElements(){
   const arrayElCount = arrayElements.childElementCount
   for(let k=0;k<arrayElCount;k++){
-    const deleteEl = document.getElementById("generated-el-" + k)
+    const deleteEl = document.getElementById("generated-el@" + k)
     deleteEl.remove()
   }}
 
@@ -56,7 +56,6 @@ document.getElementById("generate-btn").addEventListener("click", function(){
   removeElements()
   fillArray()
   render(divs)
-  console.log(divs)
   }
 )
 
@@ -66,29 +65,21 @@ document.getElementById("sort-btn").addEventListener('click', async function(){
 
     for(let i = 0; i < divs.length; i++){
       for(let j = 0; j < ( divs.length - i -1 ); j++){
-        const current = document.getElementById("generated-el-" + j)
-        const next = document.getElementById("generated-el-" + (j+1))
-        
-
-      if(divs[j].val < divs[j+1].val){
-        console.log("swapped")
+        const current = document.getElementById("generated-el@" + divs[j].id)
+        const next = document.getElementById("generated-el@" + (divs[j+1].id))
           current.style.backgroundImage="linear-gradient(to right, green,#303234,green)"
           next.style.backgroundImage="linear-gradient(to right, green,#303234,green)"
-          await delay(1000)
+          await delay(100)
           current.style.backgroundImage="linear-gradient(to right,#00BFFF,#9400D3)"
           next.style.backgroundImage="linear-gradient(to right,#00BFFF,#9400D3)"
-          let temp = divs[j]
-          divs[j]=divs[j+1]
-          divs[j+1]=temp
-          let tempArr = []
-          copyArray(divs, tempArr)
-          removeElements()
-          copyArray(tempArr, divs)
-          console.log(divs)
-          tempArr = []
-          render(divs)
-          
-        }
+            if(divs[j].val < divs[j+1].val){
+                let temp = divs[j]
+                divs[j]=divs[j+1]
+                divs[j+1]=temp
+                removeElements()
+                render(divs)
+                
+              }
             }
     }
    
